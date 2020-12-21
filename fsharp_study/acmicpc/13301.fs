a60d476fdd3f69fd0aac4dfb652ae8789835059a
drmgil44 <drmgil44@gmail.com>
2020-12-21 13:57:37 +0900

    Update 13301.fs

--- fsharp_study/acmicpc/13301.fs
+++ fsharp_study/acmicpc/13301.fs
@@ -1,13 +1,15 @@
 open System
 
-module BasicFunctions =
-    let rec getResult n cnt x y =
-        if cnt=n then (x+y)*2
+[<EntryPoint>]
+let main argv = 
+    let rec getResult n cnt (x:bigint) (y:bigint) =
+        if cnt=n then (x+y)*(new Numerics.BigInteger(2))
         else if x > y then getResult n (cnt+1) x (x+y)
         else getResult n (cnt+1) (x+y) y
 
     let getRes n =
-        getResult n 1 1 1
+        getResult n 1 (new Numerics.BigInteger(1)) (new Numerics.BigInteger(1))
 
-    Console.ReadLine() |> Convert.ToInt32 |> getRes |> printfn "%d"
+    Console.ReadLine() |> Convert.ToInt32 |> getRes |> printfn "%A"
 
+    0 // return an integer exit code
